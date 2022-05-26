518ce204ae2a1e9e92c48389df061be2877aee0f
mukeshreddyramgari6132 <99046551+mukeshreddyramgari6132@users.noreply.github.com>
2022-05-26 16:49:59 +0530

    Add files via upload

---
+++ binary.py
@@ -0,0 +1,20 @@
+def binarysearch(li,target):
+    low=0
+    high=len(li)-1
+    while low<=high:
+        mid=(low+high)//2
+        if li[mid]==target:
+            return mid
+        else:
+            if mid<target:
+                low=mid
+            else:
+                high=mid
+li=[1,3,5,6,7,8,90,3454,4567,7890,98765]
+binarysearch(li,4567)
+#linear searching in datastuctures with python
+def linear(li,target):    
+    for i in range(len(li)):
+        if li[i]==target:
+            return i
+linear(li,7)
